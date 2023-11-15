df60c7b33b73f8293ac830dfe2ef6a05a4883113
m4rcer <m4rcer@gmail.com>
2023-11-15 23:15:12 +0300

    Добавьте файлы проекта.

---
+++ MyPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_lab3
+{
+    public class MyPoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public MyPoint(double x, double y)
+        {
+            this.X = x; this.Y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0};{1}", this.X, this.Y);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace math_lab3
+{
+   
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            /*double[] xs1 = new double[] { 1, 3, 6, 8 };
+            double[] ys1 = new double[] { 2, 4, 1, 6 };*/
+            var points = new List<MyPoint> { new MyPoint(1, 2), new MyPoint(2, 3), new MyPoint(4, 1), new MyPoint(7, 4) };
+
+            var result = Cubic.InterpolateXY(points.ToArray(), 100);
+
+            Console.WriteLine("");
+        }
+    }
+}
